86c7ab021b799554fb3b670131af522a081e96b9
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-12-27 09:25:40 +0500

    Refactored data_rec script.

--- PoC_data_rec.py
+++ PoC_data_rec.py
@@ -6,16 +6,17 @@ Created on Tue Dec 20 09:43:02 2022
 """
 
 import time
-import zmq
 from random import randrange
+
+import zmq
 
 ctx = zmq.Context()
 
-# Streaming tyemprature of the all states
+# Streaming temperature of the all states
 sock_data_pub = ctx.socket(zmq.PUB)
 sock_data_pub.bind("tcp://*:5557")
 
-# Getting Feedback from client 'NY' if temprature is high
+# Getting Feedback from client 'NY' if temperature is high
 sock_data_sub = ctx.socket(zmq.SUB)
 sock_data_sub.connect("tcp://localhost:5550")
 sock_data_sub.setsockopt_string(zmq.SUBSCRIBE, '')
@@ -31,7 +32,7 @@ while True:
     time.sleep(0.0001)
     zipcode = randrange(10000, 10009)
     temperature = randrange(100, 135)
-    relhumidity = str(randrange(10, 60)) * 100000  #randrange(10, 60)
+    relhumidity = str(randrange(10, 60)) * 100000  # randrange(10, 60)
     time_record = time.time_ns()
 
     if zipcode == 10001 and temperature > 120:
@@ -47,27 +48,23 @@ while True:
         print("Condition met, expect an increment in Cluster")
 
     sock_data_pub.send_string(f"{zipcode} {time_record} {temperature} {relhumidity}")
-    
+
     # For GUI
     try:
         string = sock_data_sub.recv_string(flags=zmq.NOBLOCK)
         zip_filter, pub_time, temp, relhumidity = string.split()
         rec_.append([pub_time, time.time_ns(), zip_filter, temp, relhumidity])
-        
-        # If temprature is greater than 120, stream back to the weather station (data Rec) for concern
+
+        # If temperature is greater than 120, stream back to the weather station (data Rec) for concern
         # if int(temp) > 120:
         #     print((f"Warning TEMP at state {zip_filter} becomes: {temp}"))
-    
+
     except zmq.Again:
         pass
-    
+
     record += 1
     if record == 2000:
         break
-
-
-
-
 
 rr = rec_[0]
 float(rr[1]) - float(rr[0])
